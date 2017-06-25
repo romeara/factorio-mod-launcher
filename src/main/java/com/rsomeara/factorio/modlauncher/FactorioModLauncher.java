605414dfdde29e437824900e4a2c61ba8301fa67
Ryan O'Meara <romeara@live.com>
2017-06-25 11:54:14 -0400

    Add main application entry point

---
+++ src/main/java/com/rsomeara/factorio/modlauncher/FactorioModLauncher.java
@@ -0,0 +1,24 @@
+package com.rsomeara.factorio.modlauncher;
+
+/**
+ * Main entry point of the application, starts the launcher and creates the
+ * interface
+ *
+ * @author romeara
+ * @since 0.1.0
+ */
+public class FactorioModLauncher {
+
+	/**
+	 * Called by external commands to start the Java program
+	 *
+	 * @param args
+	 *            Any arguments provided on the command line when starting the
+	 *            application
+	 * @since 0.1.0
+	 */
+	public static void main(String[] args) {
+		// TODO Implement program logic
+	}
+
+}
