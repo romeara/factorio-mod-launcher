266d71f6fd8dd0a4bd4ac0f6d92f789dbdec3777
Ryan O'Meara <romeara@live.com>
2017-07-01 18:53:13 -0400

    Add more documentation and tests for the FilePathService

--- src/main/java/com/rsomeara/factorio/modlauncher/service/IFilePathService.java
+++ src/main/java/com/rsomeara/factorio/modlauncher/service/IFilePathService.java
@@ -10,12 +10,28 @@ import java.nio.file.Path;
  */
 public interface IFilePathService {
 
+    /**
+     * @return Path to the Factorio directory where mod files are stored
+     * @since 0.1.0
+     */
     Path getFactorioModsDirectory();
 
+    /**
+     * @return Path to the file Factorio reads from to determined if mods are enabled or disabled
+     * @since 0.1.0
+     */
     Path getFactorioModList();
 
+    /**
+     * @return Path to the directory where all mod launcher specific information is stored
+     * @since 0.1.0
+     */
     Path getModPacksDirectory();
 
+    /**
+     * @return The path to the mod pack launcher's properties file, where persisted data is stored
+     * @since 0.1.0
+     */
     Path getPropertiesFile();
 
 }
---
+++ src/test/java/com/rsomeara/factorio/modlauncher/test/service/impl/FilePathServiceTest.java
@@ -0,0 +1,54 @@
+package com.rsomeara.factorio.modlauncher.test.service.impl;
+
+import java.nio.file.Path;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.rsomeara.factorio.modlauncher.service.impl.FilePathService;
+
+public class FilePathServiceTest {
+
+    private final FilePathService filePathService = new FilePathService();
+
+    @Test
+    public void getFactorioModsDirectory() throws Exception {
+        Path factorioModsDirectory = filePathService.getFactorioModsDirectory();
+
+        Assert.assertNotNull(factorioModsDirectory);
+
+        // No matter the OS, the final part of the path is "mods"
+        Assert.assertEquals(factorioModsDirectory.getFileName().toString(), "mods");
+    }
+
+    @Test
+    public void getFactorioModsList() throws Exception {
+        Path factorioModsDirectory = filePathService.getFactorioModList();
+
+        Assert.assertNotNull(factorioModsDirectory);
+
+        // No matter the OS, the final part of the path is "mod-list.json"
+        Assert.assertEquals(factorioModsDirectory.getFileName().toString(), "mod-list.json");
+    }
+
+    @Test
+    public void getModPacksDirectory() throws Exception {
+        Path factorioModsDirectory = filePathService.getModPacksDirectory();
+
+        Assert.assertNotNull(factorioModsDirectory);
+
+        // No matter the OS, the final part of the path is ".modPacks"
+        Assert.assertEquals(factorioModsDirectory.getFileName().toString(), ".modPacks");
+    }
+
+    @Test
+    public void getPropertiesFile() throws Exception {
+        Path factorioModsDirectory = filePathService.getPropertiesFile();
+
+        Assert.assertNotNull(factorioModsDirectory);
+
+        // No matter the OS, the final part of the path is ".modPacks"
+        Assert.assertEquals(factorioModsDirectory.getFileName().toString(), "launcher.properties");
+    }
+
+}
