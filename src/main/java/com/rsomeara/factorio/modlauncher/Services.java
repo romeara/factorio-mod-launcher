2eed4cc302801b5e079ef8b28409654367e91f4f
Ryan O'Meara <romeara@live.com>
2017-07-01 12:01:20 -0400

    Basic back-end create/edit-name functions

--- src/main/java/com/rsomeara/factorio/modlauncher/Services.java
+++ src/main/java/com/rsomeara/factorio/modlauncher/Services.java
@@ -1,13 +1,15 @@
 package com.rsomeara.factorio.modlauncher;
 
+import com.rsomeara.factorio.modlauncher.service.IFactorioService;
 import com.rsomeara.factorio.modlauncher.service.IFilePathService;
 import com.rsomeara.factorio.modlauncher.service.IModPackService;
+import com.rsomeara.factorio.modlauncher.service.impl.FactorioService;
 import com.rsomeara.factorio.modlauncher.service.impl.FilePathService;
 import com.rsomeara.factorio.modlauncher.service.impl.ModPackService;
 
 /**
- * Simple substituted for a full-blown dependency injection system. Frameworks like Spring and Guice require more
- * finangling to play nice with JavaFX's injection, and the size of this app doesn't yet lend to such effort
+ * Simple substitute for a full-blown dependency injection system. Frameworks like Spring and Guice require more
+ * finagaling to play nice with JavaFX's injection, and the size of this app doesn't yet lend to such effort
  *
  * @author romeara
  * @since 0.1.0
@@ -18,9 +20,12 @@ public final class Services {
 
     private static IModPackService modPackService;
 
+    private static IFactorioService factorioService;
+
     static {
         filePathService = new FilePathService();
         modPackService = new ModPackService(filePathService);
+        factorioService = new FactorioService(filePathService);
     }
 
     public static IFilePathService getFilePathService() {
@@ -29,6 +34,10 @@ public final class Services {
 
     public static IModPackService getModPackService() {
         return modPackService;
+    }
+
+    public static IFactorioService getFactorioService() {
+        return factorioService;
     }
 
 }
--- src/main/java/com/rsomeara/factorio/modlauncher/service/impl/ModPackService.java
+++ src/main/java/com/rsomeara/factorio/modlauncher/service/impl/ModPackService.java
@@ -4,9 +4,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Base64;
+import java.util.List;
 import java.util.Objects;
 import java.util.Properties;
+import java.util.stream.Collectors;
 
+import com.google.common.base.Preconditions;
+import com.rsomeara.factorio.modlauncher.Services;
 import com.rsomeara.factorio.modlauncher.service.IFilePathService;
 import com.rsomeara.factorio.modlauncher.service.IModPackService;
 
@@ -25,8 +29,14 @@ public final class ModPackService implements IModPackService {
     }
 
     @Override
-    public void create(String name) throws IOException {
+    public String create(String name) throws IOException {
         Objects.requireNonNull(name);
+        name = name.trim();
+
+        Preconditions.checkArgument(!name.isEmpty(), "Cannot create mod pack with empty name");
+
+        // Check for duplicates
+        Preconditions.checkArgument(!getAll().contains(name), "Mod pack with name '" + name + "' already exists");
 
         // Create initial mod-pack from current
         String encodedName = Base64.getEncoder().encodeToString(name.getBytes());
@@ -41,10 +51,54 @@ public final class ModPackService implements IModPackService {
         Files.copy(filePathService.getFactorioModList(), modPackPath);
 
         // Save the current modpack as the active modpack
-        setCurrentModPack(encodedName);
+        setActive(name);
+
+        return name;
     }
 
-    private void setCurrentModPack(String encodedName) throws IOException {
+    @Override
+    public List<String> getAll() throws IOException {
+        return Files.list(Services.getFilePathService().getModPacksDirectory())
+                .map(Path::getFileName)
+                .map(Object::toString)
+                .filter(input -> input.endsWith(".json"))
+                .map(input -> input.substring(0, input.length() - 5))
+                .map(input -> Base64.getDecoder().decode(input))
+                .map(String::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getActive() throws IOException {
+        Properties props = new Properties();
+        props.load(Files.newBufferedReader(Services.getFilePathService().getPropertiesFile()));
+
+        String currentEncoded = props.getProperty("current.modpack");
+
+        return new String(Base64.getDecoder().decode(currentEncoded));
+    }
+
+    @Override
+    public void delete(String name) throws IOException {
+        Objects.requireNonNull(name);
+
+        Preconditions.checkArgument(!Objects.equals(name, getActive()), "Cannot delete enabled mod pack");
+
+        String encodedName = Base64.getEncoder().encodeToString(name.getBytes());
+
+        Path modPackPath = filePathService.getModPacksDirectory().resolve(encodedName + ".json");
+
+        if (Files.exists(modPackPath)) {
+            Files.delete(modPackPath);
+        }
+    }
+
+    @Override
+    public void setActive(String name) throws IOException {
+        Objects.requireNonNull(name);
+
+        String encodedName = Base64.getEncoder().encodeToString(name.getBytes());
+
         if (!Files.exists(filePathService.getPropertiesFile())) {
             Files.createFile(filePathService.getPropertiesFile());
         }
